30dc6477f686b2ceed8f755a6b37a9fba3eddbed
bil <bil092014@gmail.com>
2019-08-25 15:56:58 -0400

    upgrade

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,51 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  selector: "app-create-employee",
+  templateUrl: "./create-employee.component.html",
+  styleUrls: ["./create-employee.component.css"]
 })
 export class CreateEmployeeComponent implements OnInit {
-  
   employeeForm: FormGroup;
-  fullNameLength : number;
+  fullNameLength: number;
+  updateExperience: boolean = false;
 
   // This object contains all the validation messages for this form
   validationMessages = {
-    'fullName': {
-      'required': 'Full Name is required.',
-      'minlength': 'Full Name must be greater than 2 characters.',
-      'maxlength': 'Full Name must be less than 10 characters.'
+    fullName: {
+      required: "Full Name is required.",
+      minlength: "Full Name must be greater than 2 characters.",
+      maxlength: "Full Name must be less than 15 characters."
     },
-    'email': {
-      'required': 'Email is required.'
+    email: {
+      required: "Email is required."
     },
-    'phone': {
-      'required': 'Phone is required.'
+    phone: {
+      required: "Phone is required."
     },
-    'skillName': {
-      'required': 'Skill Name is required.',
+    skillName: {
+      required: "Skill Name is required."
     },
-    'experienceInYears': {
-      'required': 'Experience is required.',
+    experienceInYears: {
+      required: "Experience is required."
     },
-    'proficiency': {
-      'required': 'Proficiency is required.',
-    },
+    proficiency: {
+      required: "Proficiency is required."
+    }
   };
 
   // This object will hold the messages to be displayed to the user
   // Notice, each key in this object has the same name as the corresponding form control
   formErrors = {
-    'fullName': '',
-    'email': '',
-    'skillName': '',
-    'experienceInYears': '',
-    'proficiency': ''
+    fullName: "",
+    email: "",
+    skillName: "",
+    experienceInYears: "",
+    proficiency: ""
   };
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     // Avec FormGroup & FormControl
@@ -60,54 +65,64 @@ export class CreateEmployeeComponent implements OnInit {
     // });
     // Avec formBuilder
     this.employeeForm = this.fb.group({
-      fullName: [ ''/*1er indice pour default value*/ , [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      fullName: [
+        "" /*1er indice pour default value*/,
+        [Validators.required, Validators.minLength(2), Validators.maxLength(15)]
+      ],
       // Si un seul validator alors pas la peine de le mettre dans un array, ça sera comme ça --> ['', Validators.required]
-      contactPreference: ['email'],
-      email: ['', Validators.required],
-      phone: [''],
+      contactPreference: ["email"],
+      email: ["", Validators.required],
+      phone: [""],
       skills: this.fb.group({
-        skillName: ['', Validators.required],
-        experienceInYears: ['', Validators.required],
-        proficiency: ['', Validators.required]
+        skillName: ["", Validators.required],
+        experienceInYears: ["", Validators.required],
+        proficiency: ["", Validators.required]
       })
     });
 
-    // subscribing to valueChanges. This method is exposed in AbstractControl so inherited by FormGroup and FormControl. 
+    // subscribing to valueChanges. This method is exposed in AbstractControl so inherited by FormGroup and FormControl.
     // it returns Observable<any>
-    this.employeeForm.get('fullName').valueChanges.subscribe((value: string) => {
-      this.fullNameLength = value.length;
-    });
+    this.employeeForm
+      .get("fullName")
+      .valueChanges.subscribe((value: string) => {
+        this.fullNameLength = value.length;
+      });
 
-    this.employeeForm.valueChanges.subscribe((data) => {
+    this.employeeForm.valueChanges.subscribe(data => {
       this.logValidationErrors(this.employeeForm);
     });
 
-    this.employeeForm.get('contactPreference').valueChanges.subscribe((valRadioSelected : string) => {
-      this.onContactPreferenceChange(valRadioSelected);
-    });
-  }// End ngOnInit()
+    this.employeeForm
+      .get("contactPreference")
+      .valueChanges.subscribe((valRadioSelected: string) => {
+        this.onContactPreferenceChange(valRadioSelected);
+      });
+  } // End ngOnInit()
 
-  //cette methode souscrit a valuesChange (vois ds ngOnInt)
-  logValidationErrors(group : FormGroup = this.employeeForm){
+  //cette methode souscrit a valuesChange (voir ngOnInt)
+  logValidationErrors(group: FormGroup = this.employeeForm) {
     Object.keys(group.controls).forEach(key => {
-      
       const abstractControl = group.get(key);
 
-      if(abstractControl instanceof FormGroup){
+      if (abstractControl instanceof FormGroup) {
         this.logValidationErrors(abstractControl);
-      }else{
+      } else {
         //effacer d'abord message historique
-        this.formErrors[key] = '';
+        this.formErrors[key] = "";
 
-        if(abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)){         
+        if (
+          abstractControl &&
+          !abstractControl.valid &&
+          (abstractControl.touched || abstractControl.dirty)
+        ) {
           const messages = this.validationMessages[key];
 
-          //console.log(abstractGroup.errors) si par exemple abstractGroup concerne fullName 
+          //console.log(abstractGroup.errors) si par exemple abstractGroup concerne fullName
           //et fullname pas renseigné --> {required: true}
 
-          for(const errorKey /*exemple required*/ in abstractControl.errors){
-            if(errorKey){
-              this.formErrors[key] += messages[errorKey] + ' ';
+          for (const errorKey /*exemple required*/ in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + " ";
             }
           }
         }
@@ -117,15 +132,27 @@ export class CreateEmployeeComponent implements OnInit {
 
   ChargerDataWithPatchValueAndSetValue(): void {
     this.employeeForm.setValue({
-      fullName: 'moi',
-      email: 'moi@com',
-
+      fullName: "me",
+      email: "me@com",
+      phone: "100-100-1010",
+      contactPreference: "phone",
       skills: {
-        skillName: 'All',
+        skillName: "angular-react-js-c#",
         experienceInYears: 2,
-        proficiency: 'intermediate'
+        proficiency: "intermediate"
       }
     });
+    this.updateExperience = !this.updateExperience;
+  }
+
+  UpdateExperienceWithPatchValue(): void {
+    this.employeeForm.patchValue({
+      skills: {
+        experienceInYears: 5,
+        proficiency: "advanced"
+      }
+    });
+    this.updateExperience = !this.updateExperience;
   }
 
   loopAndLogThrougFormGroup(group: FormGroup): void {
@@ -136,24 +163,24 @@ export class CreateEmployeeComponent implements OnInit {
         this.loopAndLogThrougFormGroup(abstractControl);
         // abstractControl.disable(); si on veut desactiver uniquement le nested group
       } else {
-        console.log('key = ' + key, 'Value = ' + abstractControl.value);
+        console.log("key = " + key, " | Value = " + abstractControl.value);
         abstractControl.disable();
-        // abstractControl.markAsDirty() ...etc
+        //abstractControl.markAsPending(); ...etc
       }
     });
   }
 
-  logFormErrors(group : FormGroup){
+  logFormErrors(group: FormGroup) {
     this.logValidationErrors(group); //il va remplis formErrors
     console.log(this.formErrors);
   }
 
-  onContactPreferenceChange(valRadioButtonSelected : string){
-    const phoneControl = this.employeeForm.get('phone');
-    if(valRadioButtonSelected === 'phone'){
-      phoneControl.setValidators([Validators.required]) //method in AbsractControl
-    }else{
-      phoneControl.clearValidators() //method in AbsractControl
+  onContactPreferenceChange(valRadioButtonSelected: string) {
+    const phoneControl = this.employeeForm.get("phone");
+    if (valRadioButtonSelected === "phone") {
+      phoneControl.setValidators([Validators.required]); //method in AbsractControl
+    } else {
+      phoneControl.clearValidators(); //method in AbsractControl
     }
     phoneControl.updateValueAndValidity(); //pour appliquer les changement immediatement (method in AbsractControl)
   }
